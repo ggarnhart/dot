d963a98b8f6166767763564614caa987a00d97b4
Greg Garnhart <gegarnhart@email.wm.edu>
2019-10-28 23:35:29 -0400

    Updates

---
+++ precache-manifest.f96a2982a24b282bf4c524d807a9c893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5384cacff3ac760fdc9d6dae373fc660",
+    "url": "/dot/index.html"
+  },
+  {
+    "revision": "63814d6dc989c36c8f38",
+    "url": "/dot/static/css/main.07ce66dc.chunk.css"
+  },
+  {
+    "revision": "afadd992942c428e2c27",
+    "url": "/dot/static/js/2.0b1af0b1.chunk.js"
+  },
+  {
+    "revision": "63814d6dc989c36c8f38",
+    "url": "/dot/static/js/main.c82b5da8.chunk.js"
+  },
+  {
+    "revision": "fe5a1ce684054fd94e88",
+    "url": "/dot/static/js/runtime-main.a7e94c75.js"
+  }
+]);
